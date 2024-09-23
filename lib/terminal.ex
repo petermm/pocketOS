6e084538d0d7a1ce9ddcdfbf03658790d06d2487
Davide Bettio <davide@uninstall.it>
2024-09-23 23:45:21 +0200

    Add Terminal app
    Signed-off-by: Davide Bettio <davide@uninstall.it>

---
+++ lib/terminal.ex
@@ -0,0 +1,186 @@
+alias PhotonUI.Widgets.Container
+alias PhotonUI.Widgets.TextInputState
+alias PhotonUI.UIServer
+alias UI.Terminal.TermWidget
+
+defmodule UI.Terminal do
+  defstruct [:fpid, :fref, :pre_input_buffer, :pre_input_col_pos, line_buffer: ""]
+
+  @compile {:no_warn_undefined, :arepl}
+
+  @ui [
+    %Container{
+      name: :grid,
+      x: 0,
+      y: 0,
+      width: 320,
+      height: 240,
+      children: [
+        %TermWidget{
+          name: :term_widget,
+          x: 1,
+          y: 1,
+          height: 240,
+          width: 320
+        }
+      ]
+    }
+  ]
+
+  def start_link(args, opts) do
+    UIServer.start_link(__MODULE__, args, opts)
+  end
+
+  def init(_opts) do
+    leader = self()
+
+    spawn(fn ->
+      :erlang.group_leader(leader, self())
+      :arepl.start()
+    end)
+
+    {:ok, {@ui, %{}}, %__MODULE__{}}
+  end
+
+  def handle_call(_msg, _from, state) do
+    {:reply, :error, state}
+  end
+
+  def handle_cast(_msg, state) do
+    {:noreply, state}
+  end
+
+  def handle_info({:io_request, fpid, fref, request}, ui, state) do
+    io_request(request, fpid, fref, ui, state)
+  end
+
+  def handle_info(_msg, _ui, state) do
+    {:noreply, state}
+  end
+
+  def handle_event(:term_widget, {:got_char, ?\n}, ui, state) do
+    %__MODULE__{line_buffer: line_buffer, fpid: fpid, fref: fref} = state
+
+    {new_line_buffer, _edit_pos} =
+      TextInputState.update_text(line_buffer, ?\n, byte_size(line_buffer))
+
+    updated_ui =
+      do_unicode_write([?\n], ui)
+      |> UIServer.begin_widget_state_update()
+      |> UIServer.update_property!(:term_widget, :accepting_input, false)
+      |> UIServer.apply_widget_state_update(ui)
+
+    reply = {:io_reply, fref, :unicode.characters_to_list(new_line_buffer)}
+    send(fpid, reply)
+
+    {:noreply, updated_ui, %__MODULE__{state | line_buffer: "", fpid: nil, fref: nil}}
+  end
+
+  def handle_event(:term_widget, {:got_char, char}, ui, state) do
+    %__MODULE__{
+      line_buffer: line_buffer,
+      pre_input_buffer: pre_input_buffer,
+      pre_input_col_pos: pre_input_col_pos
+    } = state
+
+    {new_line_buffer, _edit_pos} =
+      TextInputState.update_text(line_buffer, char, byte_size(line_buffer))
+
+    pre_input_buffer_ui =
+      ui
+      |> UIServer.begin_widget_state_update()
+      |> UIServer.update_property!(:term_widget, :buffer, pre_input_buffer)
+      |> UIServer.update_property!(:term_widget, :cursor_col_pos, pre_input_col_pos)
+      |> UIServer.apply_widget_state_update(ui)
+
+    updated_ui = do_unicode_write(new_line_buffer, pre_input_buffer_ui)
+
+    {:noreply, updated_ui, %__MODULE__{state | line_buffer: new_line_buffer}}
+  end
+
+  def handle_event(name, what, _ui, state) do
+    :erlang.display({:handle_event, name, what})
+    {:noreply, state}
+  end
+
+  def io_request({:get_line, :unicode, data}, fpid, fref, ui, state) do
+    updated_ui =
+      do_unicode_write(data, ui)
+      |> UIServer.begin_widget_state_update()
+      |> UIServer.update_property!(:term_widget, :accepting_input, true)
+      |> UIServer.apply_widget_state_update(ui)
+
+    pre_input_buffer = UIServer.get_property!(updated_ui, :term_widget, :buffer)
+    pre_input_col_pos = UIServer.get_property!(updated_ui, :term_widget, :cursor_col_pos)
+
+    {:noreply, updated_ui,
+     %__MODULE__{
+       state
+       | fpid: fpid,
+         fref: fref,
+         pre_input_buffer: pre_input_buffer,
+         pre_input_col_pos: pre_input_col_pos
+     }}
+  end
+
+  def io_request({:put_chars, :unicode, data}, fpid, fref, ui, state) do
+    updated_ui = do_unicode_write(data, ui)
+    send(fpid, {:io_reply, fref, :ok})
+    {:noreply, updated_ui, state}
+  end
+
+  defp split_lines(lines_list, initial_col) do
+    columns = 40
+
+    chunk_fun = fn char, {count, rchars} ->
+      cond do
+        char == ?\n -> {:cont, Enum.reverse(rchars), {0, []}}
+        count == columns -> {:cont, Enum.reverse(rchars), {1, [char]}}
+        true -> {:cont, {count + 1, [char | rchars]}}
+      end
+    end
+
+    after_fun = fn
+      {_count, []} -> {:cont, [], []}
+      {_count, rchars} -> {:cont, Enum.reverse(rchars), []}
+    end
+
+    Enum.chunk_while(lines_list, {initial_col, []}, chunk_fun, after_fun)
+  end
+
+  defp do_unicode_write(unicode_bin, ui) when is_binary(unicode_bin) do
+    unicode_bin
+    |> :unicode.characters_to_list()
+    |> do_unicode_write(ui)
+  end
+
+  defp do_unicode_write(unicode_list, ui) do
+    old_buffer = UIServer.get_property!(ui, :term_widget, :buffer)
+    old_cursor_col_pos = UIServer.get_property!(ui, :term_widget, :cursor_col_pos)
+
+    reverse_chunks =
+      unicode_list
+      |> split_lines(old_cursor_col_pos)
+      |> Enum.reduce([], fn line, acc -> [:unicode.characters_to_binary(line) | acc] end)
+
+    [last_line | other_lines] = old_buffer
+    first_new_line = List.last(reverse_chunks)
+    other_new_lines = Enum.slice(reverse_chunks, 0, length(reverse_chunks) - 1)
+
+    new_buffer =
+      (other_new_lines ++ [last_line <> first_new_line | other_lines])
+      |> Enum.slice(0, 15)
+
+    [new_last_line | _] = new_buffer
+    # TODO: String.length
+    cursor_col_pos =
+      new_last_line
+      |> :unicode.characters_to_list()
+      |> length()
+
+    UIServer.begin_widget_state_update(ui)
+    |> UIServer.update_property!(:term_widget, :buffer, new_buffer)
+    |> UIServer.update_property!(:term_widget, :cursor_col_pos, cursor_col_pos)
+    |> UIServer.apply_widget_state_update(ui)
+  end
+end
---
+++ lib/terminal/term_widget.ex
@@ -0,0 +1,74 @@
+alias UI.Terminal.TermWidgetState
+
+defmodule UI.Terminal.TermWidget do
+  @enforce_keys [:name]
+  defstruct [:name, :x, :y, :width, :height]
+
+  @bg_color 0xFFFFFF
+  @state_struct TermWidgetState
+
+  def can_be_focused?(_widget, _state), do: true
+  def state_struct_module(), do: @state_struct
+
+  def render(term_widget, name, ui_state, origin_x, origin_y, acc) do
+    %__MODULE__{x: x, y: y} = term_widget
+
+    %{buffer: buffer, cursor_col_pos: cursor_col_pos, accepting_input: accepting_input} =
+      ui_state[name]
+
+    x_pos = origin_x + x
+    y_pos = origin_y + y
+
+    row_count = length(buffer)
+    last_row = max(row_count - 1, 0)
+
+    {_counter, list} =
+      Enum.reduce(buffer, {last_row, acc}, fn text, {row_index, rows_acc} ->
+        {row_index - 1,
+         [
+           {:text, x_pos, y_pos + 16 * row_index, :default16px, 0x000000, @bg_color, text}
+           | rows_acc
+         ]}
+      end)
+
+    cursor_color =
+      if ui_state[:"$focused_item"] == name do
+        if accepting_input do
+          0x000000
+        else
+          0x808080
+        end
+      else
+        0x202020
+      end
+
+    [{:rect, x_pos + cursor_col_pos * 8, y_pos + last_row * 16, 8, 16, cursor_color} | list]
+  end
+end
+
+defmodule UI.Terminal.TermWidgetState do
+  defstruct buffer: [""],
+            cursor_col_pos: 0,
+            accepting_input: false
+
+  def update_property(%__MODULE__{} = s, property, value) do
+    case property do
+      :buffer ->
+        %__MODULE__{s | buffer: value}
+
+      :cursor_col_pos ->
+        %__MODULE__{s | cursor_col_pos: value}
+
+      :accepting_input ->
+        %__MODULE__{s | accepting_input: value}
+    end
+  end
+
+  def handle_input(term_state, {:keyboard, :down, code}, _ts) do
+    {term_state, [event: {:got_char, code}]}
+  end
+
+  def handle_input(text_input_state, event, ts) do
+    PhotonUI.UIServer.default_input_handler(text_input_state, event, ts)
+  end
+end
