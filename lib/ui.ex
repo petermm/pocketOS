0bc485668af9b02cd0d81933e7f5349df7c69f8e
Davide Bettio <davide@uninstall.it>
2024-10-20 23:03:41 +0200

    UI: launch and monitor applications
    Signed-off-by: Davide Bettio <davide@uninstall.it>

--- lib/ui.ex
+++ lib/ui.ex
@@ -1,3 +1,5 @@
+alias PhotonUI.Widgets.ImageState
+
 defmodule UI do
   @bg_color 0xFFFFFF
 
@@ -6,6 +8,7 @@ defmodule UI do
   end
 
   def init(opts) do
+    Process.register(self(), :ui_server)
     :erlang.send_after(1, self(), :show_hello)
     {:ok, Enum.into(opts, %{})}
   end
@@ -24,29 +27,61 @@ defmodule UI do
       {:rect, 0, 0, width, height, @bg_color}
     ]
 
-    :erlang.send_after(3000, self(), :show_settings)
+    :erlang.send_after(3000, self(), {:show, UI.Menu})
 
     {:noreply, state, [{:push, items}]}
   end
 
-  def handle_info(:show_settings, %{display_server: display} = state) do
+  def handle_info({:show, what}, %{display_server: display} = state) do
     IO.puts("settings")
 
     # TODO: keyboard_server might be optional, let's handle it as optional
     %{width: width, height: height, keyboard_server: keyboard_server} = state
 
     # TODO: same here, keyboard_server might be optional
-    {:ok, settings} =
-      UI.Settings.start_link(Enum.into(state, []),
-        display_server: display,
-        height: height,
-        width: width,
-        keyboard_server: keyboard_server
-      )
+    {:ok, {settings, _settings_ref}} =
+      apply(what, :start_monitor, [
+        Enum.into(state, []),
+        [
+          display_server: display,
+          height: height,
+          width: width,
+          keyboard_server: keyboard_server
+        ]
+      ])
 
     PhotonUI.UIServer.show(settings)
 
     {:noreply, state}
+  end
+
+  defp error_description(foo) do
+    {"Error:", inspect(foo)}
+  end
+
+  defp error_description(%exception_type{} = ex) do
+    title = "#{exception_type}:"
+    message = Exception.message(ex)
+    {title, message}
+  end
+
+  def handle_info({:DOWN, _ref, :process, _pid, :normal, state}) do
+    {:noreply, state}
+  end
+
+  def handle_info({:DOWN, ref, :process, pid, {err_data, _}}, state) do
+    error_image = ImageState.load_image({:pocket_os, "icons/status/critical.rgba"})
+    {title, message} = error_description(err_data)
+
+    error_display = [
+      {:text, 16, 16, :default16px, 0xFFFFFF, 0x404040, title},
+      {:text, 16, 32, :default16px, 0xFFFFFF, 0x404040, message},
+      {:text, 16, 240 - 32, :default16px, 0xFFFFFF, 0x404040, "Press any key to continue."},
+      {:image, div(320 - 64, 2), div(240 - 64, 2), 0x404040, error_image},
+      {:rect, 0, 0, 320, 240, 0x404040}
+    ]
+
+    {:noreply, state, [push: error_display]}
   end
 
   def handle_info(msg, state) do
