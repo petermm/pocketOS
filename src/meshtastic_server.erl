b0717de1c75496d1cb0732e3a848bfa0dcd9e12b
Davide Bettio <davide@uninstall.it>
2024-11-10 21:48:58 +0100

    meshtastic: discard duplicated packets
    Signed-off-by: Davide Bettio <davide@uninstall.it>

--- src/meshtastic_server.erl
+++ src/meshtastic_server.erl
@@ -14,7 +14,9 @@
     handle_info/2
 ]).
 
--record(state, {radio, callbacks}).
+-record(state, {radio, callbacks, last_seen = #{}}).
+
+-define(PACKET_SEEN_EXPIRY_SEC, 30).
 
 start_link(Radio, MeshtasticOpts) ->
     gen_server:start_link(?MODULE, [Radio, MeshtasticOpts], []).
@@ -28,15 +30,28 @@ init([Radio, MeshtasticOpts]) ->
 
 handle_call({handle_payload, {_IfaceId, _Pid}, Payload, _Attributes}, _From, State) ->
     case meshtastic:parse(Payload) of
-        {ok, #{hop_limit := _HopLimit} = Packet} ->
-            case meshtastic:decrypt(Packet) of
-                #{data := Decrypted} = DecryptedPacket ->
-                    Message = meshtastic_proto:decode(Decrypted),
-                    DecodedPacket = DecryptedPacket#{message => Message},
-                    maybe_callback(State#state.callbacks, message_cb, DecodedPacket),
-                    {reply, ok, State};
-                Undecryptable ->
-                    io:format("Failed meshtastic decrypt: ~p.~n", [Undecryptable]),
+        {ok, #{hop_limit := _HopLimit, src := Src, packet_id := PacketId} = Packet} ->
+            MonotonicTS = erlang:monotonic_time(second),
+            {Duplicated, UpdatedLastSeen} = update_last_seen(
+                State#state.last_seen, Src, PacketId, MonotonicTS
+            ),
+            PrunedLastSeen = prune_expired_last_seen(UpdatedLastSeen, MonotonicTS),
+            if
+                not Duplicated ->
+                    case meshtastic:decrypt(Packet) of
+                        #{data := Decrypted} = DecryptedPacket ->
+                            Message = meshtastic_proto:decode(Decrypted),
+                            DecodedPacket = DecryptedPacket#{message => Message},
+                            maybe_callback(State#state.callbacks, message_cb, DecodedPacket),
+                            {reply, ok, State#state{last_seen = PrunedLastSeen}};
+                        Undecryptable ->
+                            % We don't update last seen when we receive a corrupt packet
+                            % just in case next retransmision is ok
+                            io:format("Failed meshtastic decrypt: ~p.~n", [Undecryptable]),
+                            {reply, discard, State}
+                    end;
+                true ->
+                    io:format("Discarding duplicated: ~p.~n", [PacketId]),
                     {reply, discard, State}
             end;
         _SomethingElse ->
@@ -55,3 +70,39 @@ maybe_callback(undefined, _, _) ->
     ok;
 maybe_callback(Mod, Callback, Arg) ->
     Mod:Callback(Arg).
+
+update_last_seen(LastSeenMap, Source, PacketId, MonotonicSec) ->
+    {AlreadySeen, UpdatedLastSeenMap} =
+        case LastSeenMap of
+            #{Source := #{PacketId := LastTimestamp} = SeenPacketMap} ->
+                if
+                    MonotonicSec > LastTimestamp ->
+                        {true, LastSeenMap#{Source => SeenPacketMap#{PacketId => MonotonicSec}}};
+                    true ->
+                        {true, LastSeenMap}
+                end;
+            #{Source := SeenPacketMap} ->
+                {false, LastSeenMap#{Source => SeenPacketMap#{PacketId => MonotonicSec}}};
+            _ ->
+                {false, LastSeenMap#{Source => #{PacketId => MonotonicSec}}}
+        end.
+
+prune_expired_last_seen(LastSeenMap, MonotonicSec) ->
+    PrunedMap =
+        maps:map(
+            fun(_Source, SeenPacketMap) ->
+                maps:filter(
+                    fun(PacketId, LastTimestamp) ->
+                        if
+                            LastTimestamp + ?PACKET_SEEN_EXPIRY_SEC < MonotonicSec ->
+                                false;
+                            true ->
+                                true
+                        end
+                    end,
+                    SeenPacketMap
+                )
+            end,
+            LastSeenMap
+        ),
+    maps:filter(fun(_Source, SeenPacketMap) -> SeenPacketMap =/= #{} end, PrunedMap).
--- test/meshtastic_server_test.exs
+++ test/meshtastic_server_test.exs
@@ -32,5 +32,26 @@ defmodule MeshtasticServerTest do
 
     payload_1 = <<0>>
     :next = :meshtastic_server.handle_payload(server, iface, payload_1, %{rssi: -20, snr: 15})
+
+    :ok = :gen_server.stop(server)
+    Process.unregister(:meshtastic_server_tester)
+  end
+
+  test "duplicates are discarded" do
+    Process.register(self(), :meshtastic_server_tester)
+
+    {:ok, server} = :meshtastic_server.start_link(:undefined, callbacks: TestCallbacks)
+
+    iface = {:mock, :undefined}
+
+    payload_0 =
+      <<255, 255, 255, 255, 132, 70, 49, 67, 214, 65, 11, 101, 99, 31, 0, 0, 128, 234, 186, 192,
+        51, 252, 136, 181, 75, 187, 247, 119, 124, 9, 148, 58, 83, 188, 81, 148>>
+
+    :ok = :meshtastic_server.handle_payload(server, iface, payload_0, %{rssi: -28, snr: 11})
+    :discard = :meshtastic_server.handle_payload(server, iface, payload_0, %{rssi: -10, snr: 12})
+
+    :ok = :gen_server.stop(server)
+    Process.unregister(:meshtastic_server_tester)
   end
 end
